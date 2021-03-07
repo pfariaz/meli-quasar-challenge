ca8921f2b16a88617e7d109711debc0664867a20
pfariaz <pfariaz011@gmail.com>
2021-03-07 13:02:47 -0300

    chore(swagger): add swagger documentation

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/pfariaz/meli-quasar-challenge
 go 1.16
 
 require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/gin-gonic/gin v1.6.3 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/swaggo/files v0.0.0-20190704085106-630677cd5c14
+	github.com/swaggo/gin-swagger v1.3.0
+	github.com/swaggo/swag v1.5.1 // indirect
 )
--- routes/generic.go
+++ routes/generic.go
@@ -3,12 +3,25 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/pfariaz/meli-quasar-challenge/controllers"
+	"github.com/pfariaz/meli-quasar-challenge/docs"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func GenericRoutes(router *gin.Engine) {
+
+	// programmatically set swagger info
+	docs.SwaggerInfo.Title = "Quasar Fire Meli Test API"
+	docs.SwaggerInfo.Description = "This is MercadoLibre test API"
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "quasarmeli.swagger.io"
+	docs.SwaggerInfo.BasePath = "/v1"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+
 	generic := router.Group("")
 	{
 		generic.GET("/", controllers.GetHealthcheck)
+		generic.GET("/api-docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	}
 }
