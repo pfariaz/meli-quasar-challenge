8f690eec5e2beff9051f145f5ee8509cdb6b72c2
pfariaz <pfariaz011@gmail.com>
2021-03-09 11:45:41 -0300

    chore(location): get satellites postions from map loaded

--- services/location.go
+++ services/location.go
@@ -1,6 +1,10 @@
 package services
 
-import "math"
+import (
+	"math"
+
+	"github.com/pfariaz/meli-quasar-challenge/lib"
+)
 
 // input: distancia al emisor tal cual se recibe en cada satélite
 // output: las coordenadas ‘x’ e ‘y’ del emisor del mensaje
@@ -13,18 +17,18 @@ func GetLocation(distances ...float64) (x, y float64) {
 
 	//Kenobi info
 	kenobiDistance := distances[0]
-	var positionXKenobi float64 = -500
-	var positionYKenobi float64 = -200
+	var positionXKenobi float64 = SatellitesLocation[lib.KenobiSatelliteName].X
+	var positionYKenobi float64 = SatellitesLocation[lib.KenobiSatelliteName].Y
 
 	//Skywalker info
 	skywalkerDistance := distances[1]
-	var positionXSkywalker float64 = 100
-	var positionYSkywalker float64 = -100
+	var positionXSkywalker float64 = SatellitesLocation[lib.SkywalkerSatelliteName].X
+	var positionYSkywalker float64 = SatellitesLocation[lib.SkywalkerSatelliteName].Y
 
 	//Sato info
 	satoDistance := distances[2]
-	var positionXSato float64 = 500
-	var positionYSato float64 = 100
+	var positionXSato float64 = SatellitesLocation[lib.SatoSatelliteName].X
+	var positionYSato float64 = SatellitesLocation[lib.SatoSatelliteName].Y
 
 	x = ((((math.Pow(kenobiDistance, 2)-math.Pow(skywalkerDistance, 2))+(math.Pow(positionXSkywalker, 2)-math.Pow(positionXKenobi, 2))+(math.Pow(positionYSkywalker, 2)-math.Pow(positionYKenobi, 2)))*(2*positionYSato-2*positionYSkywalker) - ((math.Pow(skywalkerDistance, 2)-math.Pow(satoDistance, 2))+(math.Pow(positionXSato, 2)-math.Pow(positionXSkywalker, 2))+(math.Pow(positionYSato, 2)-math.Pow(positionYSkywalker, 2)))*(2*positionYSkywalker-2*positionYKenobi)) / ((2*positionXSkywalker-2*positionXSato)*(2*positionYSkywalker-2*positionYKenobi) - (2*positionXKenobi-2*positionXSkywalker)*(2*positionYSato-2*positionYSkywalker)))
 
