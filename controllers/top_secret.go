30f4f25f1f00211979a37f320093928a7146b57e
pfariaz <pfariaz011@gmail.com>
2021-03-08 23:31:14 -0300

    chore(swagger): add info endpoints

--- controllers/top_secret.go
+++ controllers/top_secret.go
@@ -7,25 +7,20 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/pfariaz/meli-quasar-challenge/config"
+	"github.com/pfariaz/meli-quasar-challenge/lib"
 	"github.com/pfariaz/meli-quasar-challenge/models"
 	"github.com/pfariaz/meli-quasar-challenge/models/schemas"
 	"github.com/pfariaz/meli-quasar-challenge/services"
 )
 
-func GetSatellitesNames() []string {
-	return []string{"kenobi", "skywalker", "sato"}
-}
-
-func Contains(s []string, str string) bool {
-	for _, v := range s {
-		if v == str {
-			return true
-		}
-	}
-
-	return false
-}
-
+// ProcessMessageLocation godoc
+// @Summary Process Message given info by all satellites
+// @Param satellites body schemas.ProcessFullMessageRequestSchema true "Add satellites info"
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} schemas.ShipResponseSchema
+// @Failure 400 {object} schemas.HTTPError
+// @Router /topsecret/ [post]
 func ProcessMessageLocation(c *gin.Context) {
 	var request schemas.ProcessFullMessageRequestSchema
 
@@ -36,7 +31,7 @@ func ProcessMessageLocation(c *gin.Context) {
 
 	qtySatellites := len(request.Sattelites)
 	if qtySatellites < 3 || qtySatellites > 3 {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": "We need to provide the information of the 3 known sattelites (kenobi, skywalker and sato)"})
+		c.JSON(http.StatusBadRequest, schemas.HTTPError{Error: "We need to provide the information of the 3 known sattelites (kenobi, skywalker and sato)"})
 		return
 	}
 
@@ -49,8 +44,8 @@ func ProcessMessageLocation(c *gin.Context) {
 		messages = append(messages, sattelite.Message)
 	}
 
-	if !reflect.DeepEqual(givenSattelites, GetSatellitesNames()) {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": "we cannot determine the location since we receive unknown satellites (known satellites are kenobi, skywalker and sato)"})
+	if !reflect.DeepEqual(givenSattelites, services.GetSatellitesNames()) {
+		c.JSON(http.StatusBadRequest, schemas.HTTPError{Error: "we cannot determine the location since we receive unknown satellites (known satellites are kenobi, skywalker and sato)"})
 		return
 	}
 
@@ -68,22 +63,31 @@ func ProcessMessageLocation(c *gin.Context) {
 	c.JSON(http.StatusOK, messageLocationShip)
 }
 
+// ProcessPartialMessageLocation godoc
+// @Summary Process Message given info by each satellite
+// @Param satellite_name path string true "string enums" Enums(kenobi, skywalker, sato)
+// @Param satellite body schemas.ProcessSplitMessageRequestSchema true "Add satellite info"
+// @Accept  json
+// @Produce  json
+// @Success 204
+// @Failure 400 {object} schemas.HTTPError
+// @Router /topsecret_split/{satellite_name} [post]
 func ProcessPartialMessageLocation(c *gin.Context) {
 	var request schemas.ProcessSplitMessageRequestSchema
 	satelliteName := c.Param("satellite_name")
 	if err := c.ShouldBind(&request); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, schemas.HTTPError{Error: err.Error()})
 		return
 	}
 
-	if !Contains(GetSatellitesNames(), satelliteName) {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": "We need you to provide us with the information of the 3 known satellites (kenobi, skywalker or sato)"})
+	if !lib.Contains(services.GetSatellitesNames(), satelliteName) {
+		c.JSON(http.StatusBadRequest, schemas.HTTPError{Error: "We need you to provide us with the information of the 3 known satellites (kenobi, skywalker or sato)"})
 		return
 	}
 
-	// Create satellite
+	// Create satelliteInfo
 	messageJoined := strings.Join(request.Message, ",")
-	satellite := models.Satellite{Name: satelliteName, Distance: request.Distance, Message: messageJoined}
+	satellite := models.SatelliteMessage{Name: satelliteName, Distance: request.Distance, Message: messageJoined}
 	config.DB.Where("name = ?", satelliteName).First(&satellite)
 
 	if satellite.ID != 0 {
@@ -97,12 +101,20 @@ func ProcessPartialMessageLocation(c *gin.Context) {
 	c.Writer.WriteHeader(http.StatusNoContent)
 }
 
+// GetPartialMessageLocation godoc
+// @Summary Get location given info from 3 satellites
+// @Param satellite_name path string true "string enums" Enums(kenobi, skywalker, sato)
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} schemas.ShipResponseSchema
+// @Failure 400 {object} schemas.HTTPError
+// @Router /topsecret_split/{satellite_name} [get]
 func GetPartialMessageLocation(c *gin.Context) {
-	var satellitesCreated []models.Satellite
+	var satellitesCreated []models.SatelliteMessage
 	config.DB.Find(&satellitesCreated)
 
-	if len(satellitesCreated) < len(GetSatellitesNames()) {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": "To determine exact message and position, we need the information of more satellites"})
+	if len(satellitesCreated) < len(services.GetSatellitesNames()) {
+		c.JSON(http.StatusBadRequest, schemas.HTTPError{Error: "To determine exact message and position, we need the information of more satellites"})
 		return
 	}
 	var distances []float64
