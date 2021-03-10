e04897e589b24e5b21064fc78275829c758ebfa1
pfariaz <pfariaz011@gmail.com>
2021-03-09 21:19:26 -0300

    chore(swagger): set host and basepath

--- routes/generic.go
+++ routes/generic.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/pfariaz/meli-quasar-challenge/controllers"
 	"github.com/pfariaz/meli-quasar-challenge/docs"
@@ -10,12 +13,14 @@ import (
 
 func GenericRoutes(router *gin.Engine) {
 
+	completeHost := fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT"))
+
 	// programmatically set swagger info
 	docs.SwaggerInfo.Title = "Quasar Fire Meli Test API"
 	docs.SwaggerInfo.Description = "This is MercadoLibre test API"
 	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Host = "quasarmeli.swagger.io"
-	docs.SwaggerInfo.BasePath = "/v1"
+	docs.SwaggerInfo.Host = completeHost
+	docs.SwaggerInfo.BasePath = "/api/v1"
 	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 
 	generic := router.Group("")
