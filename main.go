6aade32419ab4a750c2bc90b63e85fda524c5d8a
pfariaz <pfariaz011@gmail.com>
2021-03-09 23:18:38 -0300

    chore(swagger): move to main method sagger definition

--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"github.com/pfariaz/meli-quasar-challenge/config"
+	"github.com/pfariaz/meli-quasar-challenge/docs"
+	"github.com/pfariaz/meli-quasar-challenge/lib"
 	"github.com/pfariaz/meli-quasar-challenge/routes"
 	"github.com/pfariaz/meli-quasar-challenge/services"
 )
 
 func main() {
+
+	// programmatically set swagger info
+	docs.SwaggerInfo.Title = "Quasar Fire Meli Test API"
+	docs.SwaggerInfo.Description = "This is MercadoLibre test API"
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = lib.GetBaseURL()
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	docs.SwaggerInfo.Schemes = []string{"https"}
+
 	r := routes.InitializeRoutes()
 	services.LoadSatellites()
 	config.ConnectDatabase()
--- routes/generic.go
+++ routes/generic.go
@@ -3,26 +3,17 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/pfariaz/meli-quasar-challenge/controllers"
-	"github.com/pfariaz/meli-quasar-challenge/docs"
-	"github.com/pfariaz/meli-quasar-challenge/lib"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func GenericRoutes(router *gin.Engine) {
-
-	// programmatically set swagger info
-	docs.SwaggerInfo.Title = "Quasar Fire Meli Test API"
-	docs.SwaggerInfo.Description = "This is MercadoLibre test API"
-	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Host = lib.GetBaseURL()
-	docs.SwaggerInfo.BasePath = "/api/v1"
-	docs.SwaggerInfo.Schemes = []string{"https"}
-
 	generic := router.Group("")
 	{
 		generic.GET("/", controllers.GetHealthcheck)
-		generic.GET("/api-docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+		url := ginSwagger.URL("/api-docs/doc.json")
+		generic.GET("/api-docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 
 	}
 }
