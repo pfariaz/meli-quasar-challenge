dc6673db7cf0e0a66a6dbb7dcf94311867a716c4
pfariaz <pfariaz011@gmail.com>
2021-03-11 00:57:29 -0300

    chore(db): get url by environment

--- config/db.go
+++ config/db.go
@@ -1,6 +1,9 @@
 package config
 
 import (
+	"os"
+
+	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/pfariaz/meli-quasar-challenge/models"
@@ -8,8 +11,18 @@ import (
 
 var DB *gorm.DB
 
+func GetDBName() string {
+	environment := os.Getenv("GIN_MODE")
+	db_url := "meli-quasar.db"
+	if environment == gin.TestMode {
+		db_url = "meli-testing-quasar.db"
+	}
+
+	return db_url
+}
+
 func ConnectDatabase() {
-	database, err := gorm.Open("sqlite3", "meli-quasar.db")
+	database, err := gorm.Open("sqlite3", GetDBName())
 
 	if err != nil {
 		panic("Failed to connect to database!")
@@ -18,5 +31,4 @@ func ConnectDatabase() {
 	database.AutoMigrate(&models.SatelliteMessage{})
 
 	DB = database
-
 }
